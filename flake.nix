4b58002b03c783103f3f673fcdc08193107024e3
Simon Pedersen <simonp203@gmail.com>
2025-03-15 13:21:08 +0100

    adding nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Flake for clusterio";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    # importing package example
+    # packages."x86_64-linux".default =
+    #   pkgs.callPackage (import ./default.nix) {};
+
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      packages = [pkgs.pnpm];
+    };
+  };
+}
